3789c3dcd9d0e2a4b42dd6b39e3cc1e38ed59f85
IkhlasulAmal <Ikhlasulamal660@gmail.com>
2026-05-30 20:34:03 +0700

    amal

---
+++ MechanicalFaRm/App/Models/m_Barang.cs
@@ -0,0 +1,14 @@
+﻿namespace database_mechanical_farm.Models
+{
+    public class m_Barang
+    {
+        public int id_barang { get; set; }
+        public int id_users { get; set; }
+
+        public string nama_barang { get; set; }
+        public string deskripsi { get; set; }
+
+        public int harga_sewa { get; set; }
+        public decimal stok { get; set; }
+    }
+}
